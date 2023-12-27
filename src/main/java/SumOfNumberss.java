d4ef8740c0cdc4be3bd3c634e284d7a4e08f6e74
pannu46 <arpandeep6330@gmail.com>
2023-12-26 21:21:02 -0500

    commiting pom.xml and sum of numbers

---
+++ src/main/java/SumOfNumberss.java
@@ -0,0 +1,14 @@
+
+public class SumOfNumberss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 10;
+		int sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
